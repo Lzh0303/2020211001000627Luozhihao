cf85f5cee2c3758a70e6a72aacf581f78b49cde6
LuozhiHao <2438080621@qq.com>
2022-06-12 20:18:01 +0800

    week 15 code

---
+++ src/main/java/com/luozhihao/controller/AdminOrderList.java
@@ -0,0 +1,44 @@
+package com.luozhihao.controller;
+
+import com.luozhihao.dao.OrderDao;
+import com.luozhihao.model.Order;
+import com.luozhihao.model.Payment;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.List;
+
+@WebServlet(name = "AdminOrderList",value = "/admin/orderList")
+public class AdminOrderList extends HttpServlet {
+    private Connection con = null;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        con = (Connection) getServletContext().getAttribute("con");
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Payment> paymentTypeList = Payment.findAllPayment(con);
+        request.setAttribute("paymentTypeList",paymentTypeList);
+        OrderDao orderDao = new OrderDao();
+        List<Order> orderList = orderDao.findAll(con);
+        request.setAttribute("orderList",orderList);
+        String path = "/WEB-INF/views/admin/orderList.jsp";
+        request.getRequestDispatcher(path).forward(request,response);
+    }
+}
